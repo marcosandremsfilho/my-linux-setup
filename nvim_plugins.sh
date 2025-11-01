2544d496738283fe541d7c31417c5b47dc25fa1c
marcosandremsfilho <marcos32andre@gmail.com>
2025-11-01 14:14:50 -0300

    feat: new script that checks if the nvim is installed and install the plugins

---
+++ nvim_plugins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if command -v nvim >/dev/null 2>&1; then
+    echo "Installing npm ..."
+    sudo apt install npm
+
+    echo "installing ripgrep ..."
+    sudo apt install ripgrep # This dependencie is used to enable the live grep from telescope
+
+    echo "installing xclip ..."
+    sudo apt install xclip # This dependencie is used to enable the copy and paste from outside the editor
+
+    echo "Fetching the nvim plugins ..."
+
+    cp -r nvim/ ~/.config/
+else
+    echo "❌ You must install Neovim first"
+fi
+
