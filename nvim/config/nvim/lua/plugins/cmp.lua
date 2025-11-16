103b5c4e792d53b987a1a04204588300f18dc04f
marcosandremsfilho <marcos32andre@gmail.com>
2025-11-16 14:54:25 -0300

    feat: nvim: added LSP and autocomplete

---
+++ nvim/config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "buffer" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+                      if cmp.visible() then
+                        cmp.select_next_item()
+                      else
+                        fallback()
+                      end
+                    end),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                          cmp.select_prev_item()
+                        else
+                          fallback()
+                        end
+                    end),
+        }),
+      })
+    end,
+  },
+}
+
--- nvim/config/nvim/lua/plugins/lsp.lua
+++ nvim/config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,10 @@
 return {
-  -- lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-
-    end,
-  }
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = { "lua_ls", "pyright"},
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
 }
